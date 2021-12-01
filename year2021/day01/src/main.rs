8798f79d352e5d309e986061b389088c71a984ea
Brian Norman <brian.norman@datasite.com>
2021-12-01 11:21:40 -0600

    [AoC] 2021 day 1 - but make it 🌟💃functional🕺🌟

--- year2021/day01/src/main.rs
+++ year2021/day01/src/main.rs
@@ -12,16 +12,14 @@ fn part1() {
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
 
-    let mut count = 0;
-    for (prev, next) in contents.lines().tuple_windows() {
-        if (prev.parse::<i32>().unwrap() < next.parse::<i32>().unwrap()) {
-            count = count + 1;
-        }
-    }
+    let count = contents.lines()
+        .map(|x| { x.parse::<i32>().expect("Unable to parse file line as int") })
+        .tuple_windows::<(_, _)>()
+        .filter(|(prev, next)| { prev < next })
+        .count();
 
     println!("[part1] Count: {}", count);
 }
-
 
 fn part2() {
     let filename = "input.txt";
@@ -29,15 +27,11 @@ fn part2() {
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
 
-    let mut count = 0;
-    for (n1, n2, n3, n4) in contents.lines().tuple_windows::<(_, _, _, _)>() {
-        let prev = n1.parse::<i32>().unwrap() + n2.parse::<i32>().unwrap() + n3.parse::<i32>().unwrap();
-        let next = n2.parse::<i32>().unwrap() + n3.parse::<i32>().unwrap() + n4.parse::<i32>().unwrap();
-        // println!("[part2] prev={} next={}", prev, next);
-        if prev < next {
-            count = count + 1;
-        }
-    }
+    let count = contents.lines()
+        .map(|x| { x.parse::<i32>().expect("Unable to parse file line as int") })
+        .tuple_windows::<(_, _, _, _)>()
+        .filter(|(n1, n2, n3, n4)| { (n1 + n2 + n3) < (n2 + n3 + n4) })
+        .count();
 
     println!("[part2] Count: {}", count);
 }
