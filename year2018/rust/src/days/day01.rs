36f422721ca1adc3e15e756dbbed9d07ca71cf43
Brian Norman <brian.norman@datasite.com>
2021-12-21 20:02:33 -0600

    Start 2018 as separate module

---
+++ year2018/rust/src/days/day01.rs
@@ -0,0 +1,31 @@
+use std::collections::HashSet;
+
+use itertools::Itertools;
+
+pub fn part1() -> String {
+    let content = include_str!("res/input01.txt");
+    let result = content.lines()
+        .map(|v| v.parse::<isize>()
+            .expect(format!("invalid number={}", v).as_ref()))
+        .fold(0, |acc, v| acc + v);
+
+    return format!("{}", result);
+}
+
+pub fn part2() -> String {
+    let content = include_str!("res/input01.txt");
+    let numbers = content.lines()
+        .map(|v| v.parse::<isize>()
+            .expect(format!("invalid number={}", v).as_ref()))
+        .collect_vec();
+
+    let mut current = 0;
+    let mut previous = HashSet::<isize>::new();
+    previous.insert(current);
+    for i in 0.. {
+        current += numbers[i % numbers.len()];
+        if !previous.insert(current) { break; }
+    }
+
+    return format!("{}", current);
+}
---
+++ year2018/rust/src/main.rs
@@ -0,0 +1,32 @@
+use clap::{App, ErrorKind, IntoApp, Parser};
+
+use days::*;
+
+mod days;
+
+#[derive(Parser)]
+#[clap(about, version, author)]
+struct Cli {
+    #[clap()]
+    parts: Vec<String>,
+}
+
+fn main() {
+    let args: Cli = Cli::parse();
+
+    if args.parts.is_empty() {
+        let mut app: App = Cli::into_app();
+        app.error(
+            ErrorKind::MissingRequiredArgument,
+            "Most provide at least one part",
+        ).exit();
+    }
+    for part in args.parts {
+        let result = match part.as_ref() {
+            "1a" => Some(day01::part1()),
+            "1b" => Some(day01::part2()),
+            _ => None
+        };
+        println!("[{}] result: {}", part, result.unwrap_or(String::from("UNKNOWN")));
+    }
+}
