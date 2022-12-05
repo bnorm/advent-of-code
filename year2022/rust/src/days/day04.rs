26006e8ea7aff820129a34858d39efd78660f127
Brian Norman <brian.norman@datasite.com>
2022-12-05 13:02:55 -0600

    Day 4, 2022

--- year2022/rust/src/days/day04.rs
+++ year2022/rust/src/days/day04.rs
@@ -1,9 +1,39 @@
+use recap::Recap;
+use serde::Deserialize;
+
 pub fn part1() -> String {
-    let _input = include_str!("res/input04.txt").lines().collect::<Vec<_>>();
-    return format!("{:?}", 0);
+    let input = include_str!("res/input04.txt").lines().collect::<Vec<_>>();
+    let pairs = input.iter().map(|line| { line.parse::<ElfPair>().unwrap() }).collect::<Vec<_>>();
+    let count = pairs.iter().filter(|pair| { pair.overlap_completely() }).count();
+    return format!("{:?}", count);
 }
 
 pub fn part2() -> String {
-    let _input = include_str!("res/input04.txt").lines().collect::<Vec<_>>();
-    return format!("{:?}", 0);
+    let input = include_str!("res/input04.txt").lines().collect::<Vec<_>>();
+    let pairs = input.iter().map(|line| { line.parse::<ElfPair>().unwrap() }).collect::<Vec<_>>();
+    let count = pairs.iter().filter(|pair| { pair.overlap_partially() }).count();
+    return format!("{:?}", count);
+}
+
+#[derive(Debug, Copy, Clone, Hash, Eq, PartialEq, Deserialize, Recap)]
+#[recap(regex = r#"(?P<first_start>\d+)-(?P<first_end>\d+),(?P<second_start>\d+)-(?P<second_end>\d+)"#)]
+pub struct ElfPair {
+    pub first_start: i32,
+    pub first_end: i32,
+    pub second_start: i32,
+    pub second_end: i32,
+}
+
+impl ElfPair {
+    fn overlap_completely(&self) -> bool {
+        return (self.first_start <= self.second_start && self.first_end >= self.second_end) ||
+            (self.second_start <= self.first_start && self.second_end >= self.first_end);
+    }
+
+    fn overlap_partially(&self) -> bool {
+        return (self.first_start <= self.second_start && self.second_start <= self.first_end) ||
+            (self.first_start <= self.second_end && self.second_end <= self.first_end) ||
+            (self.second_start <= self.first_start && self.first_start <= self.second_end) ||
+            (self.second_start <= self.first_end && self.first_end <= self.second_end);
+    }
 }
