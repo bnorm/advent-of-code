f9f546b6c7f8131f94c0e2c204cebf8c3b4b5152
Brian Norman <brian.norman@datasite.com>
2022-12-08 07:16:47 -0600

    Day 6, 2022

--- year2022/rust/src/days/day06.rs
+++ year2022/rust/src/days/day06.rs
@@ -1,9 +1,41 @@
+use std::collections::{HashSet, VecDeque};
+
 pub fn part1() -> String {
-    let _input = include_str!("res/input06.txt").lines().collect::<Vec<_>>();
-    return format!("{:?}", 0);
+    let input = include_str!("res/input06.txt").lines().collect::<Vec<_>>();
+
+    let mut window = VecDeque::<char>::new();
+
+    let mut index = 0;
+    for (i, c) in input[0].chars().enumerate() {
+        window.push_back(c);
+        if window.len() == 4 {
+            if HashSet::<&char>::from_iter(window.iter()).len() == 4 {
+                index = i + 1;
+                break
+            }
+            window.pop_front();
+        }
+    }
+
+    return format!("{:?}", index);
 }
 
 pub fn part2() -> String {
-    let _input = include_str!("res/input06.txt").lines().collect::<Vec<_>>();
-    return format!("{:?}", 0);
+    let input = include_str!("res/input06.txt").lines().collect::<Vec<_>>();
+
+    let mut window = VecDeque::<char>::new();
+
+    let mut index = 0;
+    for (i, c) in input[0].chars().enumerate() {
+        window.push_back(c);
+        if window.len() == 14 {
+            if HashSet::<&char>::from_iter(window.iter()).len() == 14 {
+                index = i + 1;
+                break
+            }
+            window.pop_front();
+        }
+    }
+
+    return format!("{:?}", index);
 }
