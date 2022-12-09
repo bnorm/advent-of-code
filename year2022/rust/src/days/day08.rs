4226f49c83071ddc044a8c658f7fd78be77ebe03
Brian Norman <brian.norman@datasite.com>
2022-12-09 10:30:29 -0600

    Day 8, 2022

--- year2022/rust/src/days/day08.rs
+++ year2022/rust/src/days/day08.rs
@@ -1,9 +1,150 @@
+use year2022::grid::{Grid, Position};
+
 pub fn part1() -> String {
-    let _input = include_str!("res/input08.txt").lines().collect::<Vec<_>>();
-    return format!("{:?}", 0);
+    let input = include_str!("res/input08.txt").lines().collect::<Vec<_>>();
+
+    let grid = input.iter()
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_string().parse::<usize>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    let num_rows = grid.len();
+    let num_cols = grid[0].len();
+
+    let count = count(&Grid::new(grid, num_rows, num_cols));
+
+    return format!("{:?}", count);
 }
 
 pub fn part2() -> String {
-    let _input = include_str!("res/input08.txt").lines().collect::<Vec<_>>();
-    return format!("{:?}", 0);
+    let input = include_str!("res/input08.txt").lines().collect::<Vec<_>>();
+
+    let grid = input.iter()
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_string().parse::<usize>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    let num_rows = grid.len();
+    let num_cols = grid[0].len();
+
+    let scenic = most_scenic(&Grid::new(grid, num_rows, num_cols));
+
+    return format!("{:?}", scenic);
+}
+
+fn count(grid: &Grid<usize>) -> usize {
+    let mut count = 0;
+
+    for r in 0..grid.num_rows {
+        for c in 0..grid.num_cols {
+            if check_visibility(grid, &Position::new(r, c)) {
+                count += 1;
+            }
+        }
+    }
+
+    return count;
+}
+
+fn most_scenic(grid: &Grid<usize>) -> usize {
+    let mut scenic = 0;
+
+    for r in 0..grid.num_rows {
+        for c in 0..grid.num_cols {
+            let s = check_scenic(grid, &Position::new(r, c));
+            if s > scenic {
+                scenic = s;
+            }
+        }
+    }
+
+    return scenic;
+}
+
+fn check_visibility(grid: &Grid<usize>, p: &Position) -> bool {
+    if p.row == 0 || p.col == 0 || p.row == grid.num_rows - 1 || p.col == grid.num_cols - 1 {
+        return true
+    }
+
+    let height = grid[p];
+
+    let mut up = true;
+    for r in 0..p.row {
+        if grid[&Position::new(r, p.col)] >= height {
+            up = false
+        }
+    }
+    if up { return true; }
+
+    let mut down = true;
+    for r in p.row + 1..grid.num_rows {
+        if grid[&Position::new(r, p.col)] >= height {
+            down = false;
+        }
+    }
+    if down { return true; }
+
+    let mut left = true;
+    for c in 0..p.col {
+        if grid[&Position::new(p.row, c)] >= height {
+            left = false
+        }
+    }
+    if left { return true; }
+
+    let mut right = true;
+    for c in p.col + 1..grid.num_cols {
+        if grid[&Position::new(p.row, c)] >= height {
+            right = false
+        }
+    }
+    if right { return true; }
+
+    return false;
+}
+
+fn check_scenic(grid: &Grid<usize>, p: &Position) -> usize {
+    if p.row == 0 || p.col == 0 || p.row == grid.num_rows - 1 || p.col == grid.num_cols - 1 {
+        return 0
+    }
+
+    let height = grid[p];
+
+    let mut up = 0;
+    for r in (0..p.row).rev() {
+        up += 1;
+        if grid[&Position::new(r, p.col)] >= height {
+            break;
+        }
+    }
+
+    let mut down = 0;
+    for r in p.row + 1..grid.num_rows {
+        down += 1;
+        if grid[&Position::new(r, p.col)] >= height {
+            break;
+        }
+    }
+
+    let mut left = 0;
+    for c in (0..p.col).rev() {
+        left += 1;
+        if grid[&Position::new(p.row, c)] >= height {
+            break;
+        }
+    }
+
+    let mut right = 0;
+    for c in p.col + 1..grid.num_cols {
+        right += 1;
+        if grid[&Position::new(p.row, c)] >= height {
+            break;
+        }
+    }
+
+    return up * down * left * right;
 }
---
+++ year2022/rust/src/grid.rs
@@ -0,0 +1,78 @@
+use std::ops::{Index, IndexMut};
+
+#[derive(Debug, Clone, Hash, Eq, PartialEq)]
+pub struct Grid<T> {
+    values: Vec<Vec<T>>,
+    pub num_rows: usize,
+    pub num_cols: usize,
+}
+
+impl<T> Grid<T> {
+    pub fn new(values: Vec<Vec<T>>, num_rows: usize, num_cols: usize) -> Self {
+        return Grid {
+            values,
+            num_rows,
+            num_cols,
+        };
+    }
+
+    pub fn contains(&self, position: &Position) -> bool {
+        return position.row < self.num_rows && position.col < self.num_cols;
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &T> + '_ {
+        return self.values.iter().flat_map(|row| row.iter());
+    }
+}
+
+#[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
+pub struct Position {
+    pub row: usize,
+    pub col: usize,
+}
+
+impl Position {
+    pub fn new(row: usize, col: usize) -> Self {
+        Position { row, col }
+    }
+
+    pub fn neighbours(&self, num_rows: usize, num_cols: usize) -> Vec<Position> {
+        let mut result = Vec::new();
+
+        // Up
+        if self.row > 0 {
+            result.push(Position::new(self.row - 1, self.col));
+        }
+
+        // Down
+        if self.row < num_rows - 1 {
+            result.push(Position::new(self.row + 1, self.col));
+        }
+
+        // Left
+        if self.col > 0 {
+            result.push(Position::new(self.row, self.col - 1));
+        }
+
+        // Right
+        if self.col < num_cols - 1 {
+            result.push(Position::new(self.row, self.col + 1));
+        }
+
+        return result;
+    }
+}
+
+impl<T> Index<&Position> for Grid<T> {
+    type Output = T;
+
+    fn index(&self, position: &Position) -> &Self::Output {
+        return &self.values[position.row][position.col];
+    }
+}
+
+impl<T> IndexMut<&Position> for Grid<T> {
+    fn index_mut(&mut self, position: &Position) -> &mut Self::Output {
+        return &mut self.values[position.row][position.col];
+    }
+}
