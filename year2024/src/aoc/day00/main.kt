921b6ddda26f5ed44a07e42bcc6adc3ed0e18f5d
Brian Norman <brian.norman@jetbrains.com>
2024-12-07 10:15:01 -0600

    2024 day 0 (template day)

---
+++ year2024/src/aoc/day00/main.kt
@@ -0,0 +1,28 @@
+package aoc.day00
+
+import aoc.input.downloadInput
+
+const val SAMPLE1 = """
+"""
+
+const val SAMPLE2 = SAMPLE1
+
+suspend fun main() {
+    val input = downloadInput(2024, 0)
+
+    val part1 = part1(SAMPLE1)
+    require(part1 == "") { part1 }
+    println(part1(input))
+
+    val part2 = part2(SAMPLE2)
+    require(part2 == "") { part2 }
+    println(part2(input))
+}
+
+private fun part1(input: String): String {
+    TODO()
+}
+
+private fun part2(input: String): String {
+    TODO()
+}
